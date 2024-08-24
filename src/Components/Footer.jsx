6e66e5fe962b9541ede84de432bdadd3ed006c00
Keval <kevalmehta885@gmail.com>
2024-08-25 05:09:34 +0530

    Added cities and countries Navigation in AppNav

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <>
-      <footer style={{ marginTop: "auto" }}>
+      <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
         </p>
