13366a38c9a2506e04a9d6270a7f4d0f0303ead2
Keval <kevalmehta885@gmail.com>
2024-08-25 19:58:30 +0530

    Solved Few Error and updated

--- src/Components/CountryItem.jsx
+++ src/Components/CountryItem.jsx
@@ -3,26 +3,27 @@ import PropTypes from "prop-types";
 
 CountryItem.propTypes = {
   country: PropTypes.shape({
-    countryName: PropTypes.string,
-    country: PropTypes.string,
+    country: PropTypes.string.isRequired,
     emoji: PropTypes.string,
-    date: PropTypes.string,
-    notes: PropTypes.string,
-    position: PropTypes.shape({
-      lat: PropTypes.number,
-      lng: PropTypes.number,
-    }),
-    id: PropTypes.number,
-  }),
+    id: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 function CountryItem({ country }) {
   const flagemojiToPNG = (flag) => {
-    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    if (typeof flag !== "string" || flag.length === 0) {
+      return null; // Return null if the flag emoji is invalid
+    }
+
+    const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
       .map((char) => String.fromCharCode(char - 127397).toLowerCase())
       .join("");
+
     return (
-      <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+      <img
+        src={`https://flagcdn.com/24x18/${countryCode}.png`}
+        alt={`${country.country} flag`}
+      />
     );
   };
 
--- src/Components/CountryList.jsx
+++ src/Components/CountryList.jsx
@@ -5,18 +5,26 @@ import PropTypes from "prop-types";
 import Message from "./Message";
 
 CountryList.propTypes = {
-  cities: PropTypes.array,
-  isLoading: PropTypes.bool,
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
 };
 
 function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
-  if (!CountryList.length) {
+  if (cities.length === 0) {
     return <Message message="Add your first city by clicking on Map " />;
   }
 
-  const countries = [];
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((ele) => ele.country).includes(city.country))
+      return [
+        ...arr,
+        { country: city.country, emoji: city.emoji, id: city.id },
+      ];
+    else return arr;
+  }, []);
+
   return (
     <ul className={styles.countriesList}>
       {countries.map((country) => (
